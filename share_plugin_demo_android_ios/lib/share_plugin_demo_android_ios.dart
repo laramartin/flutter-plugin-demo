1a322e890b1f76f151f18ca97956ec0a8307cfda
Lara Martin <13904500+laramartin@users.noreply.github.com>
2021-02-07 20:02:08 +0100

    Rename plugins and implement iOS

--- share_plugin_demo_android_ios/lib/share_plugin_demo_android_ios.dart
+++ share_plugin_demo_android_ios/lib/share_plugin_demo_android_ios.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -7,8 +6,7 @@ class SharePluginDemoAndroidIos {
   static const MethodChannel _channel =
       const MethodChannel('share_plugin_demo_android_ios');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<void> share(String message) async {
+    await _channel.invokeMethod('share', {'message': message});
   }
 }
